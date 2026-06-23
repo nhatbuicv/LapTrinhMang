605a42e51186fd1b82ad4c974d48dd1a9463b5c8
Nhat Bui <nhatbuicv@gmail.com>
2026-06-23 22:01:45 +0700

    Add bai 2

--- Bai1/src/TCP_Server.java
+++ Bai1/src/TCP_Server.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class TCP_Sever {
+public class TCP_Server {
     public static void main(String[] args) throws IOException {
         System.out.println("WAITING CONNECTION ...");
 
